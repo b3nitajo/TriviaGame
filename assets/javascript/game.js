bbdff46af7f2fe7d81438b05559fae0bf614b886
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-03-21 23:32:02 -0500

    psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//variables 
+
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var gameQA = [
+    {question: "fist question",answer: "first answer"},
+    {question: "second question", answer: "second answer"},
+    {question: "third question", answer: "third answer"},
+    {question: "forth question", answer: "forth answer"},
+    {question: "fifth question", answer: "fifth answer"},
+    {question: "sixth question", answer: "sixth answer"},
+    {question: "seven question", answer: "seven answer"},
+    {question: "eight question", answer: "eight answer"},
+    {question: "nine question", answer: "nine answer"},
+    {question: "ten question", answer: "ten answer"},
+];
+
+
+
+console.log(test);
+
+//FUNCTIONS
+//Calculate correct answers, 
+//incorrect answers, 
+
+function keepScore(a){
+    var newScore = a + 1; 
+    return newScore;
+}
+
+//and add option to restart the game (without reloading the page).
+
+//You'll create a trivia game that shows only one question until the player answers it 
+
+//or their time runs out.
+
+//If the player selects the correct answer, show a screen congratulating them for choosing the right option. 
+//After a few seconds, display the next question -- do this without user input.
+
+//id the player selects the wrong answers and time-outs.
+
+//If the player runs out of time
+//tell the player that time's up 
+//and display the correct answer. 
+//Wait a few seconds, then show the next question.
+
+
+//If the player chooses the wrong answer, tell the player they selected the wrong option //and then display the correct answer. 
+//Wait a few seconds, then show the next question.
+
